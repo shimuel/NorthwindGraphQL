8343c16a6ae895c8c08665818fd26a0a3dcef75f
Sam <shimuel@yahoo.com>
2019-08-20 01:00:47 -0600

    Collapsable Navbar

--- northwind-ui/src/components/Header.jsx
+++ northwind-ui/src/components/Header.jsx
@@ -1,20 +1,36 @@
 import React,{Component} from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import { NavItem, FormControl, Navbar, NavDropdown, Button, Nav, Form } from 'react-bootstrap';
 //import { LinkContainer } from "react-router-bootstrap";
 import "../App.css"
 const  Header = () => {         
   return (     
 <>
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand style={{cursor: "none"}}>Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <ul className="list-inline" style={{margin:"10px"}}>
-        <li className="list-inline-item"><NavLink className="NavLink" to="/">Home</NavLink></li>
-        <li className="list-inline-item"><NavLink className="NavLink"  to="/first">Features</NavLink></li>
-        <li className="list-inline-item"><NavLink className="NavLink"  to="/second">Price</NavLink></li>
-        <li className="list-inline-item"><NavLink className="NavLink" to="/third">About</NavLink></li>
-        <li className="list-inline-item">
+<Navbar collapseOnSelect  expand="lg">
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav className="mr-auto">
+          <Nav.Item>
+            <Nav.Link eventKey="1" as={Link} to="/Home">
+              Home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+             <Nav.Link eventKey="2" as={Link} to="/first">
+              Features
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="3" as={Link} to="/second">
+              Prices
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="3" as={Link} to="/third">
+              Other
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
           <NavDropdown title="Dropdown" className="NavDropdown" id="basic-nav-dropdown">
             <NavDropdown.Item><NavLink className="NavLink" to="/">Action</NavLink></NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -22,16 +38,17 @@ const  Header = () => {
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
           </NavDropdown>
-        </li>
-      </ul>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form>
-  </Navbar>
+          </Nav.Item>
+        </Nav>
+        <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+      </Navbar.Collapse>
+    </Navbar>
 </>
   )  
 }
 
 export default Header
+{/* <Nav className="mr-auto d-block"> */}
